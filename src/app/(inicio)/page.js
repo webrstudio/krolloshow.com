0bc7d1027038f27955ec4dd855872c3991a7c863
ricardoggz <ricardo_ggzicm@outlook.com>
2025-02-20 22:08:33 -0600

    [config]: Se limpian las carpetas y se agregan archivos iniciales

---
+++ src/app/(inicio)/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "KROLLOSHOW - INICIO",
+  description: "KROLLOSHOW",
+};
+
+export default function Home() {
+  return (
+    <main>
+      <h1>KROLLOSHOW</h1>
+      <span>lorem10</span>
+    </main>
+  );
+}
