a303f4a96f33b7a2c923ba94569181f83955710d
ricardoggz <ricardo_ggzicm@outlook.com>
2025-02-25 18:37:50 -0600

    se configura el proyecto para exportar rutas estaticas

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  generateStaticParams() {
+    return {
+      "/": { page: "/" },
+      "/tienda": {page: '/tienda'}
+    };
+  },
+};
 
 export default nextConfig;
