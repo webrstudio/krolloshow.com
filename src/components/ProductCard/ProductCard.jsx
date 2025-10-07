af98357c57558e19b4b01cb562030182833afadc
webrstudio <contacto@webrstudio.com>
2025-10-06 18:39:26 -0600

    se agrega el numero de piezas disponibles a cada producto

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -8,16 +8,16 @@ import { LiaShoppingCartSolid } from "react-icons/lia";
 
 export const ProductCard = ({ product }) => {
   const { addProduct } = useContext(ShoppingCartContext);
-  const showModal = ()=>{
+  const showModal = () => {
     Swal.fire({
-      title:'Añadido al carrito',
-      icon:'success',
-      timer:1000,
-      position:'top-end',
-      showConfirmButton:false
-    })
-    return addProduct(product)
-  }
+      title: "Añadido al carrito",
+      icon: "success",
+      timer: 1000,
+      position: "top-end",
+      showConfirmButton: false,
+    });
+    return addProduct(product);
+  };
   return (
     <>
       {!product ? null : (
@@ -26,28 +26,30 @@ export const ProductCard = ({ product }) => {
             <img src={product.producto_imagen} />
           </figure>
           <span>{product.producto_nombre}</span>
+          <span>
+            Únicamente <strong>{product.producto_inventario}</strong> pieza(s)
+            disponibles
+          </span>
           <span>${product.producto_precio}.00</span>
-          {
-            !product.producto_adicionales ? null
-            :
-            <span className={styles.productAditionals}>{product.producto_adicionales}</span>
-          }
+          {!product.producto_adicionales ? null : (
+            <span className={styles.productAditionals}>
+              {product.producto_adicionales}
+            </span>
+          )}
           <Link
             href={`/pago/${product.producto_id}`}
             className={styles.productPaymentButton}
           >
             Comprar ahora
           </Link>
-          {
-            /*
+          {/*
               <button
                 className={styles.productShoppingCartButton}
                 onClick={showModal}
               >
                 <LiaShoppingCartSolid />
               </button>
-            */
-          }
+            */}
         </div>
       )}
     </>
